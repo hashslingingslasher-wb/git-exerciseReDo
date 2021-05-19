16aa1da4189ea3bf37ad447728b2afa07fc9a46e
Will Barlow <wjamesbarlow@gmail.com>
2021-05-19 10:58:46 -0700

    feature2

---
+++ git-exerciseReDo/src/feature2.java
@@ -0,0 +1,19 @@
+
+public class feature2 {
+
+	public static void main(String[] args) {
+		//test for feature2
+		count(feature1.RandomDiamond());
+
+	}
+		public static void count(int[] array) {
+		    int count = 0;
+		    for (int i = 0; i < array.length; i++) {
+		        if(array[i] == 0)
+		            count++;	
+		    }
+		    System.out.println("We found "+ count+ " diamond(s)!");
+		}
+	}
+	
+
