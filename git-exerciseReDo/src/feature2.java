b8c9ac82752175c6494fa0f2af27e14ef4896e8c
Will Barlow <wjamesbarlow@gmail.com>
2021-05-19 11:26:10 -0700

    Update feature2.java

--- git-exerciseReDo/src/feature2.java
+++ git-exerciseReDo/src/feature2.java
@@ -4,6 +4,7 @@ public class feature2 {
 	public static void main(String[] args) {
 		//test for both features
 		count(feature1.RandomDiamond());
+		//test comment to push
 
 	}
 		public static void count(int[] array) {
